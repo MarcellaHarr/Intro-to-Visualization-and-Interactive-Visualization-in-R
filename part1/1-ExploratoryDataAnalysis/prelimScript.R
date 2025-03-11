6488f2687641d47b914b339b4ab74586dd39a2c9
Marcella Harris <marcellalharris@yahoo.com>
2025-03-11 04:02:58 +0000

    removed renv and box libs from posit cloud

--- part1/1-ExploratoryDataAnalysis/prelimScript.R
+++ part1/1-ExploratoryDataAnalysis/prelimScript.R
@@ -1,22 +1,3 @@
-# ==============================================================================
-# Run only ONCE to initialize renv
-# ==============================================================================
-# install.packages("renv")
-# renv::init()
-
-# Install `{box}` without rebuilding every time
-# renv::install("box")
-
-# Save installed packages
-# renv::snapshot()
-
-# check installed packages
-renv::status()
-
-# Restore the correct package versions (if needed) and then load Box
-# renv::restore()
-# library(box)
-
 #==== install libraries ====
 install.packages("tidyverse")
 install.packages("ggplot2")
